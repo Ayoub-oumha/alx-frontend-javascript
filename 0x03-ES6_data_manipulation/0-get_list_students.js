89fa76eb25df720e5c6ecebbf41854de7ffcab28
Ayoub-oumha <Ayyouboumha@gmail.com>
2024-06-03 10:03:45 +0000

    commit

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,4 +1,7 @@
-let obj1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' } ;
-function getListStudents(){
-	return obj1 ;
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ] ;
 }
