786d05e6f9973f73fdcd09fef4291c90c22787f3
Ayoub-oumha <Ayyouboumha@gmail.com>
2024-06-04 16:29:19 +0000

    commit

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,9 +2,9 @@ export default function getListStudentIds(a){
     let array = new Array ;
     if(typeof(a) == "object") {
     a.map(nu => array.push(nu["id"]) )
-        console.log(array) ;
+        return array ;
     }
     else {
-        console.log(array) ;
+        return array ;
     }
 }
